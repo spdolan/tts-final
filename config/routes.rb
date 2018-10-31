f3ebd739efce1cee828b65bb7fd8068e2addf8a1
snpdolan <snpdolan@gmail.com>
2018-10-31 09:54:49 -0400

    first commit, final project blog format

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+	
+  get 'home/index'
+  resources :comments
+  devise_for :users
+  resources :posts
+
+  root 'home#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
